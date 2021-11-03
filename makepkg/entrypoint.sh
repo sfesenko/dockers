5aee122e67cf9b7960ab7b28845aac3ecace0454
Sergii Fesenko <sergii_f@outlook.com>
2021-11-03 19:03:09 +0200

    makepkg: add sysupgrade

--- makepkg/entrypoint.sh
+++ makepkg/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Do not sync packages, if cache is mounted
-test -f /var/cache/pacman/not_mounted && sudo pacman --sync --refresh
+test -f /var/cache/pacman/not_mounted && sudo pacman --sync --sysupgrade --refresh
 $*
